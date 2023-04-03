26f925cdbc66ccd0efceebfc039ce66f0f749c5b
dazehinn <dazehinn@yahoo.fr>
2023-04-03 13:59:51 +0100

    Update entry points

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
 
 import loadAPIMeals from './modules/meals/loadAPIMeals.js';
+import createPopup from './modules/comments/createPopup.js';
 
 window.onload = () => {
   loadAPIMeals();
+  createPopup();
 };
