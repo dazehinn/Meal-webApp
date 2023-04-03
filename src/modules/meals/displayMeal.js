7927fbe7550a1a8d2898f851e54c1a31737c9693
dazehinn <dazehinn@yahoo.fr>
2023-04-03 13:58:28 +0100

    Update module to diplay pop-up

--- src/modules/meals/displayMeal.js
+++ src/modules/meals/displayMeal.js
@@ -1,6 +1,13 @@
-const displayMeal = (mealCard) => {
+import loadAPIComments from '../comments/loadAPIComments.js';
+
+const displayMeal = (mealCard, meal) => {
   const mealsList = document.getElementById('meals_list');
   mealsList.appendChild(mealCard);
+
+  const commentsBtn = document.getElementById(`comments_${meal.idMeal}`);
+  commentsBtn.addEventListener('click', () => {
+    loadAPIComments(meal.idMeal);
+  });
 };
 
 export default displayMeal;
